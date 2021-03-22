6fb8e9462f290eebda6c648f778fec3901d6d347
Thomas Boda <boda.thomas@orange.fr>
2021-03-22 22:57:27 +0100

    Add react-router structure

---
+++ src/views/HomeView.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class HomeView extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    HomeView
+                </header>
+            </div>
+        )
+    }
+}
+
+export default HomeView
