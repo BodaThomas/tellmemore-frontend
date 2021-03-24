862d114f615992118da2c765b3ebe26e43050c92
Thomas Boda <boda.thomas@orange.fr>
2021-03-24 22:30:20 +0100

    Add the ability to fill the forms

--- src/components/NoteQuestion.js
+++ src/components/NoteQuestion.js
@@ -4,35 +4,46 @@ import PropTypes from 'prop-types'
 class NoteQuestion extends React.Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            value: null
+        }
+    }
+
+    handleSetValue = (e) => {
+        e.preventDefault()
+        this.setState({value: e.target.value})
+        this.props.handlerSet(e.target.value)
     }
 
     render() {
         if (this.props.answer) {
             return (
                 <div className="">
-                    <button disabled className={'border border-black cursor-default rounded-tl-md rounded-bl-md w-14 h-14 ' + (this.props.value === 1 ? 'bg-red-300 text-white font-bold' : null)}>1</button>
-                    <button disabled className={'border border-l-0 border-black cursor-default w-14 h-14 ' + (this.props.value === 2 ? 'bg-red-300 text-white font-bold' : null)}>2</button>
-                    <button disabled className={'border border-l-0 border-black cursor-default w-14 h-14 ' + (this.props.value === 3 ? 'bg-red-300 text-white font-bold' : null)}>3</button>
-                    <button disabled className={'border border-l-0 border-black cursor-default w-14 h-14 ' + (this.props.value === 4 ? 'bg-red-300 text-white font-bold' : null)}>4</button>
-                    <button disabled className={'border border-l-0 border-black cursor-default rounded-tr-md rounded-br-md w-14 h-14 ' + (this.props.value === 5 ? 'bg-red-300 text-white font-bold' : null)}>5</button>
+                    <button disabled className={'border border-black cursor-default rounded-tl-md rounded-bl-md w-14 h-14 ' + (this.props.value === 1 || this.props.value === '1' ? 'bg-red-300 text-white font-bold' : null)}>1</button>
+                    <button disabled className={'border border-l-0 border-black cursor-default w-14 h-14 ' + (this.props.value === 2 || this.props.value === '2' ? 'bg-red-300 text-white font-bold' : null)}>2</button>
+                    <button disabled className={'border border-l-0 border-black cursor-default w-14 h-14 ' + (this.props.value === 3 || this.props.value === '3' ? 'bg-red-300 text-white font-bold' : null)}>3</button>
+                    <button disabled className={'border border-l-0 border-black cursor-default w-14 h-14 ' + (this.props.value === 4 || this.props.value === '4' ? 'bg-red-300 text-white font-bold' : null)}>4</button>
+                    <button disabled className={'border border-l-0 border-black cursor-default rounded-tr-md rounded-br-md w-14 h-14 ' + (this.props.value === 5 || this.props.value === '5' ? 'bg-red-300 text-white font-bold' : null)}>5</button>
                 </div>
             )
         }
         return (
-            <div className="divide-x rounded-md border">
-                <button className={'rounded-tl-md rounded-bl-md w-14 h-14 ' + (this.props.value === 5 ? 'bg-red-300 text-white font-bold' : null)}>1</button>
-                <button className="w-14 h-14">2</button>
-                <button className="w-14 h-14">3</button>
-                <button className="w-14 h-14">4</button>
-                <button className="w-14 h-14">5</button>
+            <div className="">
+                <button value={1} onClick={this.handleSetValue} className={'border border-black rounded-tl-md rounded-bl-md w-14 h-14 ' + (this.state.value === '1' ? 'bg-red-300 text-white font-bold' : null)}>1</button>
+                <button value={2} onClick={this.handleSetValue} className={'border border-l-0 border-black w-14 h-14 ' + (this.state.value === '2' ? 'bg-red-300 text-white font-bold' : null)}>2</button>
+                <button value={3} onClick={this.handleSetValue} className={'border border-l-0 border-black w-14 h-14 ' + (this.state.value === '3' ? 'bg-red-300 text-white font-bold' : null)}>3</button>
+                <button value={4} onClick={this.handleSetValue} className={'border border-l-0 border-black w-14 h-14 ' + (this.state.value === '4' ? 'bg-red-300 text-white font-bold' : null)}>4</button>
+                <button value={5} onClick={this.handleSetValue} className={'border border-l-0 border-black rounded-tr-md rounded-br-md w-14 h-14 ' + (this.state.value === '5' ? 'bg-red-300 text-white font-bold' : null)}>5</button>
             </div>
         )
     }
 }
 
 NoteQuestion.propTypes = {
-    value: PropTypes.number,
-    answer: PropTypes.bool
+    value: PropTypes.number || PropTypes.string,
+    answer: PropTypes.bool,
+    handlerSet: PropTypes.func
 }
 
 export default NoteQuestion
--- src/views/FormView.js
+++ src/views/FormView.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-import { ChevronLeft } from 'react-feather'
+import { ChevronLeft, ArrowLeft, ArrowRight } from 'react-feather'
 import API from '../api'
+import { NoteQuestion } from '../components'
 
 
 class FormView extends React.Component {
@@ -10,9 +11,11 @@ class FormView extends React.Component {
         super(props)
 
         this.state = {
-            step: null,
+            step: 0, //SET TO 0
             name: null,
-            data: []
+            data: [],
+            result: [],
+            actualValue: null
         }
     }
 
@@ -22,8 +25,124 @@ class FormView extends React.Component {
             .then(data => this.setState({name: data.name, data: data.data}))
     }
 
+    handleSetActualValue = (value) => {
+        this.setState({actualValue: value})
+    }
+
+    handleNext = (e, type) => {
+        e.preventDefault()
+        let step = this.state.step
+        let newResult = this.state.result
+        let object
+
+        console.log(this.state.data, step)
+        if (type === 'text')
+            type = 'input'
+        if (this.state.actualValue) {
+            object = {
+                type,
+                title: this.state.data[step].value,
+                value: this.state.actualValue
+            }
+            newResult.push(object)
+            step++
+            console.log(step)
+            this.setState({step, actualValue: null, result: newResult}, () => {
+                if (step === this.state.data.length) {
+                    this.handleSubmit()
+                }
+            })
+        }
+    }
+
+    handlePrevious = (e) => {
+        e.preventDefault()
+        if (this.state.step === 0)
+            this.setState({step: null})
+        else
+            this.setState({step: this.state.step - 1})
+    }
+
+    handleSubmit = () => {
+        let body = {
+            formId: this.props.match.params.formId,
+            data: this.state.result
+        }
+
+        console.log('submit', body)
+        API.post('/addResult', body)
+            .then(json => json.data)
+            .then(data => console.log(data))
+        this.setState({step: 'final'})
+    }
+
     render() {
-        console.log(this.state.data)
+        let content
+
+        if (this.state.step === null) {
+            content = (
+                <div className="space-y-4 absolute top-1/2">
+                    <div className="text-sm">
+                        SONDAGE
+                    </div>
+                    <div className="title text-3xl font-bold">
+                        {this.state.name}
+                    </div>
+                    <div className="title font-bold text-lg">
+                        {this.state.data.length} questions
+                    </div>
+                    <div>
+                        <button onClick={() => this.setState({step: 0})} className="title bg-green-500 text-white font-bold p-2.5 rounded-md text-sm focus-within:outline-none">
+                            Commencer
+                        </button>
+                    </div>
+                </div>
+            )
+        } else if (this.state.step === 'final') {
+            content = (
+                <div className="space-y-4 absolute top-1/2">
+                    <div className="title text-3xl font-bold">
+                        Merci d&apos;avoir répondu à ce formulaire
+                    </div>
+                    <div>
+                        <Link to="/" className="title bg-green-500 text-white font-bold p-2.5 rounded-md text-sm focus-within:outline-none">
+                            Accéder à mes formulaires
+                        </Link>
+                    </div>
+                </div>
+            )
+        } else {
+            const step = this.state.step
+            let input
+
+            if (this.state.data[step] && this.state.data[step].type === 'text')
+                input = <textarea onChange={(e) => {this.setState({actualValue: e.target.value})}} className="w-2/3 p-4 text-sm rounded-md" rows={5} placeholder="Répondez ici..."/>
+            else if (this.state.data[step] && this.state.data[step].type === 'note')
+                input = <NoteQuestion handlerSet={this.handleSetActualValue}/>
+            content = (
+                <div className="space-y-4 absolute top-1/2 text-center">
+                    <div className="text-sm">
+                        QUESTION {step + 1}
+                    </div>
+                    <div className="title text-3xl font-bold">
+                        {this.state.data[step] ? this.state.data[step].value : null}
+                    </div>
+                    <div className="title">
+                        {input}
+                    </div>
+                    <div className="inline-flex bottom-0">
+                        <button onClick={this.handlePrevious} className="title inline-flex text-green-500 font-bold p-2.5 rounded-md text-sm focus-within:outline-none">
+                            <ArrowLeft className="mr-1"/>
+                            Précédent
+                        </button>
+                        <button onClick={(e) => this.handleNext(e, this.state.data[step] ? this.state.data[step].type : null)} className="title inline-flex bg-green-500 text-white font-bold p-2.5 rounded-md text-sm focus-within:outline-none">
+                            Suivant
+                            <ArrowRight className="ml-1"/>
+                        </button>
+                    </div>
+                </div>
+            )
+        }
         return (
             <div className="max-w-7xl m-auto" style={{minHeight: 'calc(100vh - 25vh)'}}>
                 <div className="grid grid-cols-3 title">
@@ -33,22 +152,7 @@ class FormView extends React.Component {
                     </Link>
                 </div>
                 <div className="bg-green-50 w-full rounded-md mt-5 p-10" style={{minHeight: 'calc(100vh - 25vh)'}}>
-                    <div className="m-auto space-y-4 absolute top-1/2">
-                        <div className="text-sm">
-                            SONDAGE
-                        </div>
-                        <div className="title text-3xl font-bold">
-                            {this.state.name}
-                        </div>
-                        <div className="title font-bold text-lg">
-                            {this.state.data.length} questions
-                        </div>
-                        <div>
-                            <button className="title bg-green-500 text-white font-bold p-2.5 rounded-md text-sm focus-within:outline-none">
-                            Commencer
-                            </button>
-                        </div>
-                    </div>
+                    {content}
                 </div>
             </div>
         )
