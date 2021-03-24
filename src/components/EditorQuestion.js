8d163016903730594f19123a36ae6d7398000cd7
Thomas Boda <boda.thomas@orange.fr>
2021-03-24 04:58:55 +0100

    Add the buttons interactable on the CreateFormView

--- src/components/EditorQuestion.js
+++ src/components/EditorQuestion.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { FileText, Star, ChevronUp, ChevronDown, Trash } from 'react-feather'
+
 
 class EditorQuestion extends React.Component {
     constructor(props) {
@@ -7,11 +10,31 @@ class EditorQuestion extends React.Component {
 
     render() {
         return (
-            <div>
-                EditorQuestion
+            <div className="w-full">
+                {
+                    this.props.data.type && this.props.data.type === 'text' ?
+                        <div className="bg-yellow-500 inline-flex p-1 pr-2 pl-2 mr-6 text-white rounded-md align-bottom">
+                            {this.props.number} - <FileText className="ml-1"/>
+                        </div>
+                        :
+                        <div className="bg-red-300 inline-flex p-1 pr-2 pl-2 mr-6 text-white rounded-md align-bottom">
+                            {this.props.number} - <Star className="ml-1"/>
+                        </div>
+                }
+                <input defaultValue={this.props.data.value} className="p-1 mr-2 inline-flex rounded-md align-bottom"/>
+                <div className="inline-flex align-bottom space-x-2 text-right">
+                    <button className="p-1 bg-white rounded-md align-bottom"><ChevronUp/></button>
+                    <button className="p-1 bg-white rounded-md align-bottom"><ChevronDown/></button>
+                    <button className="p-1 bg-white text-red-500 rounded-md align-bottom"><Trash/></button>
+                </div>
             </div>
         )
     }
+}
+
+EditorQuestion.propTypes = {
+    number: PropTypes.string,
+    data: PropTypes.array.isRequired
 }
 
 export default EditorQuestion
--- src/views/CreateFormView.js
+++ src/views/CreateFormView.js
@@ -27,6 +27,51 @@ class CreateFormView extends React.Component {
         }
     }
 
+    handleAddTextQuestion = () => {
+        let data = this.state.data
+
+        data.push({ type: 'text', value: null })
+        this.setState({data: data})
+    }
+
+    handleAddNoteQuestion = () => {
+        let data = this.state.data
+
+        data.push({ type: 'note', value: null })
+        this.setState({data: data})
+    }
+
+    handleSaveTitle = (e) => {
+        e.preventDefault()
+        if (e.target[0].value)
+            this.setState({name: e.target[0].value})
+    }
+
+    handleSave = () => {
+        if (this.state.isNewForm) {
+            API.post('/createForm', {name: this.state.name, data: this.state.data})
+                .then(json => console.log(json))
+        } else {
+            API.post('/updateForm', {id: this.props.match.params.formId, name: this.state.name, data: this.state.data})
+        }
+    }
+
+    handleDeleteForm = () => {
+        API.delete('/deleteForm?id=' + this.props.match.params.formId)
+            .then(json => {
+                console.log(json.data)
+                window.location.href = '/'
+            })
+    }
+
+    handleGoToForm = () => {
+        if (!this.state.isNewForm) {
+            window.location.href = '/form/' + this.props.match.params.formId
+        } else {
+            alert('Vous ne pouvez pas accèder à ce formulaire, car celui-ci n\'a pas encore été créé.')
+        }
+    }
+
     render() {
         return (
             <div className="min-h-screen">
@@ -36,13 +81,13 @@ class CreateFormView extends React.Component {
                             <ChevronLeft/>
                             <p className="ml-4 font-bold">Mes formulaires</p>
                         </Link>
-                        <div className="text-center">
+                        <form className="text-center" onSubmit={this.handleSaveTitle}>
                             <input className="border font-bold bg-white rounded-md p-3 mr-2 align-bottom flex-auto focus:outline-none" placeholder="Nom du formulaire" defaultValue={this.state.name}/>
-                            <button className="border p-3 text-green-500 border-green-500 bg-white rounded-md focus:outline-none"><Check/></button>
-                        </div>
+                            <button type="submit" className="border p-3 text-green-500 border-green-500 bg-white rounded-md focus:outline-none"><Check/></button>
+                        </form>
                         <div className="text-right">
-                            <button className="border p-3 text-red-500 border-red-500 bg-white rounded-md focus:outline-none mr-2"><Trash/></button>
-                            <button className="border p-3 text-white bg-green-500 border-green-500 rounded-md font-bold align-bottom focus:outline-none">Répondre</button>
+                            <button onClick={this.handleDeleteForm} className="border p-3 text-red-500 border-red-500 bg-white rounded-md focus:outline-none mr-2"><Trash/></button>
+                            <button onClick={this.handleGoToForm} className="border p-3 text-white bg-green-500 border-green-500 rounded-md font-bold align-bottom focus:outline-none">Répondre</button>
                         </div>
                     </div>
                     <div className="bg-green-50 w-full min-h-full rounded-md mt-5 p-10">
@@ -55,23 +100,26 @@ class CreateFormView extends React.Component {
                                 <div className="text-2xl font-bold text-center text-green-500">
                                     <h1>Ce formulaire est vide</h1>
                                 </div> :
-                                <div>
+                                <div className="space-y-10">
                                     {
-                                        this.state.data.map((e, i) => <EditorQuestion key={i} data={e}/>)
+                                        this.state.data.map((e, i) => {
+                                            console.log(i)
+                                            return <EditorQuestion key={i} data={e} number={i + 1}/>
+                                        })
                                     }
                                 </div>
                             }
                         </div>
                         <div>
-                            <button className="border p-3 rounded-md font-bold border-green-500 text-green-500 inline-flex focus:outline-none mr-4">
+                            <button onClick={this.handleAddTextQuestion} className="border p-3 rounded-md font-bold border-green-500 text-green-500 inline-flex focus:outline-none mr-4">
                                 <FileText className="mr-2"/> Ajouter une question &quot;Texte&quot;
                             </button>
-                            <button className="border p-3 rounded-md font-bold border-green-500 text-green-500 inline-flex focus:outline-none">
+                            <button onClick={this.handleAddNoteQuestion} className="border p-3 rounded-md font-bold border-green-500 text-green-500 inline-flex focus:outline-none">
                                 <Star className="mr-2"/> Ajouter une question &quot;Note&quot;
                             </button>
                         </div>
                         <div className="text-right">
-                            <button className="border p-3 text-white bg-green-500 border-green-500 rounded-md font-bold align-bottom focus:outline-none">
+                            <button onClick={this.handleSave} className="border p-3 text-white bg-green-500 border-green-500 rounded-md font-bold align-bottom focus:outline-none">
                                 Sauvegarder
                             </button>
                         </div>
